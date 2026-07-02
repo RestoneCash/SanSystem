19bed73bfcc860a5fa44ede380cd17a6c8358dd8
RestoneCash <398612479@qq.com>
2026-07-02 23:04:32 +0800

    7.2_未改完的实体分类

--- src/main/java/com/restonecash/sansystem/util/EntityCategory.java
+++ src/main/java/com/restonecash/sansystem/util/EntityCategory.java
@@ -2,9 +2,13 @@ package com.restonecash.sansystem.util;
 
 public enum EntityCategory
 {
-    FRIENDLY,    // 友好生物（牛、羊、村民等）
-    NEUTRAL,     // 中立生物（僵尸猪灵、末影人等，攻击条件触发）
-    HOSTILE,     // 敌对生物（僵尸、骷髅、蜘蛛等）
-    BOSS,        // BOSS生物（末影龙、凋灵等）
-    OTHER;       // 其他（如蝙蝠、鱿鱼等不重要的）
+    WEAKFRIENDLY,   // 弱小的友好生物（鱼、鸟、蝙蝠等）
+    FRIENDLY,       // 友好生物（牛、羊、村民等）
+    COMMONMOBS,     // 普通怪物（僵尸，骷髅，苦力怕，女巫，普通掠夺者等）
+    STRONGMOBS,     // 敌对生物（烈焰人，恶魂，僵尸猪人，凋零骷髅，卫道士，唤魔者，劫掠兽，末影人等）
+    ENDERDRAGON,    //末影龙
+    WITHER,         //凋零
+    WARDEN,         //坚守者
+    PLAYER,         //玩家
+    OTHER;          // 其他（以防万一）
 }
--- src/main/java/com/restonecash/sansystem/util/EntityClassificationHelper.java
+++ src/main/java/com/restonecash/sansystem/util/EntityClassificationHelper.java
@@ -5,6 +5,17 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 
 public class EntityClassificationHelper {
+    public static EntityCategory getCategory(EntityType<?> type){
+
+        if(type.equals(EntityType.PLAYER)){
+            return EntityCategory.PLAYER;
+        }
+
+        else if (type.equals(EntityType.ENDER_DRAGON)) {
+            return EntityCategory.ENDERDRAGON;
+        }
+        
+        else return EntityCategory.OTHER;
+    }
 
 }
-
