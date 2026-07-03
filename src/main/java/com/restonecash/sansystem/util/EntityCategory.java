dafd2ac553e39a66bd54feb93bd8cc88d01a66f3
RestoneCash <398612479@qq.com>
2026-07-03 12:41:51 +0800

    依旧还没搞完分类，原版tags有点少

--- src/main/java/com/restonecash/sansystem/util/EntityCategory.java
+++ src/main/java/com/restonecash/sansystem/util/EntityCategory.java
@@ -2,13 +2,14 @@ package com.restonecash.sansystem.util;
 
 public enum EntityCategory
 {
+    PLAYER,         //玩家
     WEAKFRIENDLY,   // 弱小的友好生物（鱼、鸟、蝙蝠等）
     FRIENDLY,       // 友好生物（牛、羊、村民等）
+    NEUTRAL,        // 中立生物（铁傀儡）
     COMMONMOBS,     // 普通怪物（僵尸，骷髅，苦力怕，女巫，普通掠夺者等）
-    STRONGMOBS,     // 敌对生物（烈焰人，恶魂，僵尸猪人，凋零骷髅，卫道士，唤魔者，劫掠兽，末影人等）
+    STRONGMOBS,     // 强力怪物（烈焰人，恶魂，僵尸猪人，凋零骷髅，卫道士，唤魔者，劫掠兽，末影人等）
     ENDERDRAGON,    //末影龙
     WITHER,         //凋零
     WARDEN,         //坚守者
-    PLAYER,         //玩家
     OTHER;          // 其他（以防万一）
 }
--- src/main/java/com/restonecash/sansystem/util/EntityClassificationHelper.java
+++ src/main/java/com/restonecash/sansystem/util/EntityClassificationHelper.java
@@ -11,10 +11,44 @@ public class EntityClassificationHelper {
             return EntityCategory.PLAYER;
         }
 
+        else if(type.getCategory() == MobCategory.WATER_CREATURE
+                ||type.equals(EntityType.AXOLOTL)
+                ||type.is(EntityTypeTags.BEEHIVE_INHABITORS)
+                ||type.equals(EntityType.PARROT)
+                ||type.equals(EntityType.BAT)
+                ||type.equals(EntityType.CHICKEN)
+                ||type.equals(EntityType.RABBIT)
+                ||type.equals(EntityType.FOX)
+                ||type.equals(EntityType.CAT)
+                ||type.equals(EntityType.OCELOT)){
+            return EntityCategory.WEAKFRIENDLY;
+        }
+
+        else if(type.getCategory() == MobCategory.CREATURE
+                && !type.equals(EntityType.IRON_GOLEM)){
+            return EntityCategory.FRIENDLY;
+        }
+
+        else if(type.equals(EntityType.IRON_GOLEM)){
+            return EntityCategory.NEUTRAL;
+        }
+
+        else if(){
+            return EntityCategory.COMMONMOBS;
+        }
+
         else if (type.equals(EntityType.ENDER_DRAGON)) {
             return EntityCategory.ENDERDRAGON;
         }
-        
+
+        else if (type.equals(EntityType.WITHER)) {
+            return EntityCategory.WITHER;
+        }
+
+        else if (type.equals(EntityType.WARDEN)) {
+            return EntityCategory.WARDEN;
+        }
+
         else return EntityCategory.OTHER;
     }
 
